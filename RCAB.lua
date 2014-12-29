2bfbf84e3ba961e95f935dbb2899cff71d5a38a1
Janis Born <janis.born@rwth-aachen.de>
2014-12-29 12:44:55 +0100

    rewrite to support display of remaining reagents

--- RCAB.lua
+++ RCAB.lua
@@ -1,3 +1,17 @@
+-- ActionButton local variables
+-- ============================
+-- RCAB_label: reference to the TextString for resource display
+-- RCAB_resourceCosts: table indexed by resource type (string, one of "Mana", "Rage", "Energy", "Health", "Ammo", "Reagent") with fields:
+--   amount: number
+--   name: string, name of the reagent (only in case of "Reagent" type)
+-- Example:
+-- this.RCAB_resourceCosts = { Mana = {amount = 50}, Reagent = {name = "Soul Shard", amount = 1} };
+
+-- Global variables
+-- ================
+-- RCAB_savedResourceCosts: list
+-- RCAB_savedResourceCosts[slotID]: list, RCAB_resourceCosts saved for the ActionButton with index slotID
+
 function RCAB_Init()
     -- hook ActionButton functions
     RCAB_Old_ActionButton_OnEvent = ActionButton_OnEvent;
@@ -13,71 +27,128 @@ function RCAB_ActionButton_OnEvent(event)
     RCAB_Old_ActionButton_OnEvent(event);
 
     -- ActionButton initialization
-    if not this.RCAB_initialized then
-        RCAB_ActionButton_Init();
-        this.RCAB_initialized = true;
+    if not RCAB_ActionButton_IsInitialized() then
+        RCAB_ActionButton_Initialize();
     end
-
-    if not this.RCAB_settingsLoaded or event == "ACTIONBAR_PAGE_CHANGED" then
-        -- Delayed initialization of persistent settings
+    
+    -- Restore saved settings (with delayed initialization)
+    if RCAB_savedResourceCosts and not this.RCAB_variablesLoaded then
         RCAB_ActionButton_LoadSettings();
-    elseif this.RCAB_learnedSkill then
-        -- Update counter
-        if HasAction(ActionButton_GetPagedID(this)) then
-            RCAB_ActionButton_UpdateCounter();
-        else
-            RCAB_ActionButton_ResetButton();
-        end
-    elseif event == "ACTIONBAR_SLOT_CHANGED" and arg1 == ActionButton_GetPagedID(this) then
-        -- Learn resource cost right after player placed a skill in a slot
+        this.RCAB_variablesLoaded = true;
+    end
+    
+    -- Load saved settings when switching action bars
+    if event == "ACTIONBAR_PAGE_CHANGED" then
+        RCAB_ActionButton_LoadSettings();
+    end
+    
+    -- Learn resource cost right after player placed a skill in a slot
+    if event == "ACTIONBAR_SLOT_CHANGED" and arg1 == ActionButton_GetPagedID(this) then
         RCAB_ActionButton_LearnSkill();
     end
+    
+    -- Update counter
+    RCAB_ActionButton_UpdateCounter();
 end
 
 function RCAB_ActionButton_SetTooltip()
     RCAB_Old_ActionButton_SetTooltip();
 
-    -- Check for ActionButton initialization
-    if this.RCAB_initialized then
+    if RCAB_ActionButton_IsInitialized() then
         RCAB_ActionButton_LearnSkill();
     end
 end
 
--- Internal functionality
+-- Helper functions
 
-function RCAB_ActionButton_Init()
-    -- Add the resourceLabel FontString to the ActionButton
-    this.RCAB_resourceLabel = this:CreateFontString("", "ARTWORK", "NumberFontNormalSmall");
-    this.RCAB_resourceLabel:SetPoint("BOTTOMRIGHT", this, "BOTTOMRIGHT");
-    RCAB_ActionButton_ResetButton();
+function RCAB_StripEscapeSequences(text)
+    text = string.gsub(text, "\|c%x%x%x%x%x%x%x%x", "");
+    text = string.gsub(text, "\|r", "");
+    return text;
 end
 
-function RCAB_ActionButton_ResetButton()
-    this.RCAB_resourceLabel:SetText("");
-    this.RCAB_learnedSkill = false;
+function RCAB_Merge(table1, table2)
+    for key, val in pairs(table2) do
+        table1[key] = val;
+    end
+    return table1;
+end
+
+function RCAB_Empty(t)
+    return next(t) == nil;
+end
+
+-- Internal functionality
+
+function RCAB_ActionButton_Initialize()
+    -- Add the resourceLabel FontString to the ActionButton
+    if not this.RCAB_label then
+        this.RCAB_label = this:CreateFontString("", "ARTWORK", "NumberFontNormalSmall");
+        this.RCAB_label:SetPoint("BOTTOMRIGHT", this, "BOTTOMRIGHT");
+        this.RCAB_label:SetText("");
+    end
+    
+    -- Initialize the local variable
+    if not this.RCAB_resourceCosts then
+        this.RCAB_resourceCosts = {};
+    end
+end
+
+function RCAB_ActionButton_IsInitialized()
+    return this.RCAB_label and this.RCAB_resourceCosts;
+end
+
+function RCAB_ActionButton_ShouldShowResource()
+    if HasAction(ActionButton_GetPagedID(this)) then
+        if not RCAB_Empty(this.RCAB_resourceCosts) then
+            return true;
+        end
+    end
+    return false;
 end
 
 function RCAB_ActionButton_UpdateCounter()
-    local remainingCasts = math.floor(RCAB_GetPlayerResource(this.RCAB_resourceType) / this.RCAB_resourceCost);
-    this.RCAB_resourceLabel:SetText(remainingCasts);
-    RCAB_ActionButton_UpdateTextColor();
+    if RCAB_ActionButton_ShouldShowResource() then
+        local textString = "";
+        for resourceType, info in this.RCAB_resourceCosts do
+            if resourceType == "Mana" or
+               resourceType == "Rage" or
+               resourceType == "Energy" or
+               resourceType == "Health" or
+               resourceType == "Ammo" or
+               resourceType == "Reagent" then
+                local remainingCasts = math.floor(RCAB_GetPlayerResource(resourceType) / info.cost);
+                textString = textString .. "|r ";
+                textString = textString .. RCAB_ResourceTypeColorString(resourceType);
+                textString = textString .. remainingCasts;
+            end
+        end
+        this.RCAB_label:SetText(textString);
+        this.RCAB_label:Show();
+    else
+        this.RCAB_label:Hide();
+    end
 end
 
 function RCAB_ActionButton_LearnSkill()
-    -- Try to infer resource cost and type from the GameTooltip
-    local fontString2 = getglobal("GameTooltipTextLeft2");
-    RCAB_ActionButton_LearnSkillFromResourceCost(fontString2:GetText());
-
-    local fontString1 = getglobal("GameTooltipTextLeft1");
-    RCAB_ActionButton_LearnSkillFromName(fontString1:GetText());
+    local learnedResourceCost = {};
     
-    local fontString3 = getglobal("GameTooltipTextLeft3");
-    RCAB_ActionButton_LearnSkillFromDescription(fontString3:GetText());
-
-    if this.RCAB_learnedSkill then
-        RCAB_ActionButton_SaveSettings();
-        RCAB_ActionButton_UpdateCounter();
+    for line = 1, GameTooltip:NumLines() do
+        local label = getglobal("GameTooltipTextLeft" .. line);
+        local text = RCAB_StripEscapeSequences(label:GetText());
+        if line == 1 then
+            learnedResourceCost = RCAB_Merge(learnedResourceCost, RCAB_ActionButton_LearnSkillFromName(text));
+        else
+            learnedResourceCost = RCAB_Merge(learnedResourceCost, RCAB_ActionButton_LearnSkillFromResourceCost(text));
+            learnedResourceCost = RCAB_Merge(learnedResourceCost, RCAB_ActionButton_LearnSkillFromDescription(text));
+            learnedResourceCost = RCAB_Merge(learnedResourceCost, RCAB_ActionButton_LearnSkillFromReagents(text));
+        end
     end
+        
+    this.RCAB_resourceCosts = learnedResourceCost;
+    RCAB_ActionButton_SaveSettings();
+    RCAB_RegisterForResourceEvents();
+    RCAB_ActionButton_UpdateCounter();
 end
 
 function RCAB_ActionButton_LearnSkillFromResourceCost(line)
@@ -88,13 +159,10 @@ function RCAB_ActionButton_LearnSkillFromResourceCost(line)
            resourceType == "Rage" or
            resourceType == "Energy" or
            resourceType == "Health" then
-            this.RCAB_resourceCost = resourceCost;
-            this.RCAB_resourceType = resourceType;
-            this.RCAB_learnedSkill = true;
-
-            this:RegisterEvent("UNIT_" .. string.upper(resourceType));
+            return {[resourceType] = {cost = resourceCost}};
         end
     end
+    return {};
 end
 
 function RCAB_ActionButton_LearnSkillFromName(line)
@@ -105,11 +173,10 @@ function RCAB_ActionButton_LearnSkillFromName(line)
            line == "Shoot Crossbow" or
            line == "Shoot Gun" or
            line == "Throw" then
-            this.RCAB_resourceCost = 1;
-            this.RCAB_resourceType = "Ammo";
-            this.RCAB_learnedSkill = true;
+            return {Ammo = {cost = 1}};
         end
     end
+    return {};
 end
 
 function RCAB_ActionButton_LearnSkillFromDescription(line)
@@ -117,25 +184,53 @@ function RCAB_ActionButton_LearnSkillFromDescription(line)
         -- Life Tap
         local found, _, healthCost = string.find(line, "Converts (%d+) health into %d+ mana.");
         if found then
-            this.RCAB_resourceCost = tonumber(healthCost);
-            this.RCAB_resourceType = "Health";
-            this.RCAB_learnedSkill = true;
-            this:RegisterEvent("UNIT_HEALTH");
+            return {Health = {cost = tonumber(healthCost)}};
         end
+    end
+    return {};
+end
+
+function RCAB_ActionButton_LearnSkillFromReagents(line)
+    if line ~= nil then
+        -- Single Reagent
+        line = RCAB_StripEscapeSequences(line);
+        local found, _, reagentName = string.find(line, "Reagents: ([^,]+)");
+        if found then
+            return {Reagent = {cost = 1, name = reagentName}};
+        end
+    end
+    return {};
+end
+
+function RCAB_ResourceTypeColorString(resourceType)
+    if resourceType == "Mana" then
+        return "|cFF8080FF";
+    elseif resourceType == "Rage" then
+        return "|cFFFFB366";
+    elseif resourceType == "Energy" then
+        return "|cFFFFFF66";
+    elseif resourceType == "Health" then
+        return "|cFF66FF66";
+    else
+        return "|cFFB3B3B3";
     end
 end
 
-function RCAB_ActionButton_UpdateTextColor()
-    if this.RCAB_resourceType == "Mana" then
-        this.RCAB_resourceLabel:SetTextColor(0.5, 0.5, 1.0);
-    elseif this.RCAB_resourceType == "Rage" then
-        this.RCAB_resourceLabel:SetTextColor(1.0, 0.7, 0.4);
-    elseif this.RCAB_resourceType == "Energy" then
-        this.RCAB_resourceLabel:SetTextColor(1.0, 1.0, 0.4);
-    elseif this.RCAB_resourceType == "Health" then
-        this.RCAB_resourceLabel:SetTextColor(0.4, 1.0, 0.4);
-    else
-        this.RCAB_resourceLabel:SetTextColor(0.7, 0.7, 0.7);
+function RCAB_RegisterForResourceEvents()
+    for resourceType, _ in this.RCAB_resourceCosts do
+        if resourceType == "Mana" then
+            this:RegisterEvent("UNIT_MANA");
+        elseif resourceType == "Rage" then
+            this:RegisterEvent("UNIT_RAGE");
+        elseif resourceType == "Energy" then
+            this:RegisterEvent("UNIT_ENERGY");
+        elseif resourceType == "Health" then
+            this:RegisterEvent("UNIT_HEALTH");
+        elseif resourceType == "Ammo" then
+            -- ActionButton already reacts to suitable event
+        elseif resourceType == "Reagent" then
+           this:RegisterEvent("BAG_UPDATE");
+        end
     end
 end
 
@@ -143,25 +238,20 @@ end
 
 function RCAB_ActionButton_SaveSettings()
     local slotID = ActionButton_GetPagedID(this);
-
     if not RCAB_savedResourceCosts then
         RCAB_savedResourceCosts = {};
     end
-    RCAB_savedResourceCosts[slotID] = {};
-    RCAB_savedResourceCosts[slotID].resourceCost = this.RCAB_resourceCost;
-    RCAB_savedResourceCosts[slotID].resourceType = this.RCAB_resourceType;
+    RCAB_savedResourceCosts[slotID] = this.RCAB_resourceCosts;
 end
 
 function RCAB_ActionButton_LoadSettings()
     local slotID = ActionButton_GetPagedID(this);
     if RCAB_savedResourceCosts then
         if RCAB_savedResourceCosts[slotID] then
-            this.RCAB_resourceCost = RCAB_savedResourceCosts[slotID].resourceCost;
-            this.RCAB_resourceType = RCAB_savedResourceCosts[slotID].resourceType;
-            this.RCAB_learnedSkill = true;
-            this.RCAB_settingsLoaded = true;
+            this.RCAB_resourceCosts = RCAB_savedResourceCosts[slotID];
         end
     end
+    RCAB_RegisterForResourceEvents();
 end
 
 function RCAB_GetPlayerResource(resourceType)
@@ -189,11 +279,27 @@ function RCAB_GetPlayerResource(resourceType)
         else
             return 0;
         end
+    elseif resourceType == "Reagent" then
+        return RCAB_CountReagents(this.RCAB_resourceCosts[resourceType].name);
     elseif resourceType == playerResourceType then
         return UnitMana("player");
     else
         return 0;
     end
+end
+
+function RCAB_CountReagents(reagentName)
+    local totalCount = 0;
+    for bagID = 0, 4 do
+        for slot = 1, GetContainerNumSlots(bagID) do
+            local itemLink = GetContainerItemLink(bagID, slot);
+            if itemLink and string.find(itemLink, reagentName) then
+                local _, itemCount = GetContainerItemInfo(bagID, slot);
+                totalCount = totalCount + itemCount;
+            end
+        end
+    end
+    return totalCount;
 end
 
 RCAB_Init();
